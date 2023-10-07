939251969a45ef5aa0c25811912458869f969df0
Neilansh Chauhan <neilanshchauhan4@gmail.com>
2023-10-07 12:37:00 +0530

    Modularizing Django Settings

--- manage.py
+++ manage.py
@@ -2,11 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from drfEcommerce.settings import base  
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfEcommerce.settings')
+    if base.DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfEcommerce.settings.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfEcommerce.settings.production')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
