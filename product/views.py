e51213c5641452bfd1d55cd094b0cca8f9b0ff69
Neilansh Chauhan <neilanshchauhan4@gmail.com>
2023-10-07 15:02:20 +0530

    Create Category ViewSet

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
+from .models import Category, Brand, Category       
 
 # Create your views here.
+
+class CategoryView(viewsets.ViewSet):
+    queryset = Category.objects.all()
+
+    def list(self,request):
+        serializer = CategorySerializer(self.queryset)
+        return Response(serializer.data)
