b9a9332edbef908a8ba2d9ff9119aa766c7e85aa
Neilansh Chauhan <neilanshchauhan4@gmail.com>
2023-10-07 16:48:15 +0530

    Create Routers

--- product/views.py
+++ product/views.py
@@ -1,13 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
-from .models import Category, Brand, Category       
+from .models import Category, Brand, Product     
 
 # Create your views here.
 
-class CategoryView(viewsets.ViewSet):
+class CategoryViewSet(viewsets.ViewSet):
     queryset = Category.objects.all()
 
     def list(self,request):
-        serializer = CategorySerializer(self.queryset)
+        serializer = CategorySerializer(self.queryset,many=True)
         return Response(serializer.data)
